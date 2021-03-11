7e6c573ed72709b8dc853945275440f291fb007b
Victor Queiroz <victoorqrz@gmail.com>
2021-03-11 16:07:46 -0300

    styling components

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 import Slider from '@react-native-community/slider';
 
@@ -8,6 +8,7 @@ export default function App() {
     <View style={styles.container}>
       <Image
         source={require('./src/assets/logo.png')}
+        style={styles.logo}
       />
       <Text style={styles.title}>12 caracteres</Text>
       <View style={styles.area}>
@@ -15,8 +16,19 @@ export default function App() {
           style={{height: 50}}
           minimumValue={5}
           maximumValue={15}
+          minimumTrackTintColor="#FF0000"
+          maximumTrackTintColor="#000"
         />
       </View>
+    
+      <TouchableOpacity style={styles.button} onPress={generatePass}>
+        <Text style={styles.buttonText}>Gerar senha</Text>
+      </TouchableOpacity>
+
+      <View style={styles.area}>
+        <Text style={styles.password}>12345</Text>
+      </View>
+    
     </View>
   );
 }
@@ -28,4 +40,37 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F3F3FE'
   },
+  logo: {
+    marginBottom: 60
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  area: {
+    marginTop: 15,
+    marginBottom: 15,
+    backgroundColor: '#fff',
+    width: '80%',
+    borderRadius: 7
+  },
+  button: {
+    backgroundColor: '#ffa200',
+    width: '80%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 7,
+    marginBottom: 25
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold'
+  },
+  password: {
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 20
+  }
 });
